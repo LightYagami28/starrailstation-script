98a74fd3ef9319a70f014cd3a3e70342081ed945
Lexia <147329385+exterpolation@users.noreply.github.com>
2024-01-25 22:02:30 +0100

    Fix for people that dont use firefox as their main browser

--- Pity Tracker.ps1
+++ Pity Tracker.ps1
@@ -1,7 +1,7 @@
 # [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object Net.WebClient).DownloadString("https://gist.githubusercontent.com/Star-Rail-Station/2512df54c4f35d399cc9abbde665e8f0/raw/get_warp_link_os.ps1?cachebust=srs")
 
-# Open Firefox browser to a specific URL
-Start-Process firefox https://starrailstation.com/en/warp#import
+# Open browser to a specific URL
+Start-Process https://starrailstation.com/en/warp#import
 
 # Wait for browser to load (adjust this delay if needed)
 Start-Sleep -Seconds 1
@@ -10,7 +10,7 @@ try {
     # Set TLS version to TLS 1.2 for secure communication
     [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
-    # Download and execute script from a GitHub Gist
+    # Download and execute script from the Gist
     $webClient = New-Object Net.WebClient
     $scriptUrl = "https://gist.githubusercontent.com/Star-Rail-Station/2512df54c4f35d399cc9abbde665e8f0/raw/get_warp_link_os.ps1?cachebust=srs"
     $scriptContent = $webClient.DownloadString($scriptUrl)
