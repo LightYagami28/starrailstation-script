939cb1e1d144b0bb3cfcadf95b1d9a8000cc7d6c
Light Yagami <maule2703@ik.me>
2024-04-10 21:00:02 +0200

    Update

--- Pity Tracker.ps1
+++ Pity Tracker.ps1
@@ -1,25 +1,22 @@
-# [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12; Invoke-Expression (New-Object Net.WebClient).DownloadString("https://gist.githubusercontent.com/Star-Rail-Station/2512df54c4f35d399cc9abbde665e8f0/raw/get_warp_link_os.ps1?cachebust=srs")
+# Set TLS version to TLS 1.2 for secure communication
+[Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
 
 # Open browser to a specific URL
-Start-Process https://starrailstation.com/en/warp#import
+Start-Process "https://starrailstation.com/en/warp#import"
 
 # Wait for browser to load (adjust this delay if needed)
 Start-Sleep -Seconds 1
 
 try {
-    # Set TLS version to TLS 1.2 for secure communication
-    [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor [Net.SecurityProtocolType]::Tls12
-
     # Download and execute script from the Gist
-    $webClient = New-Object Net.WebClient
     $scriptUrl = "https://gist.githubusercontent.com/Star-Rail-Station/2512df54c4f35d399cc9abbde665e8f0/raw/get_warp_link_os.ps1?cachebust=srs"
-    $scriptContent = $webClient.DownloadString($scriptUrl)
+    $scriptContent = Invoke-RestMethod -Uri $scriptUrl
 
     # Execute downloaded script
     Invoke-Expression $scriptContent
 
 } catch {
-    Write-Error "An error occurred: $_.Exception.Message"
+    Write-Error "An error occurred: $($_.Exception.Message)"
 }
 
 # Exit the script
